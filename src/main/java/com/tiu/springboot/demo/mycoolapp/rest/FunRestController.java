b9efc19ba1bede6f543fd66b0214a5dc50e38dd6
TiuDepZai <58964817+TiuDepZai@users.noreply.github.com>
2024-02-09 10:23:47 +1000

    Injection data and value access Spring-boot-dev-tools

--- src/main/java/com/tiu/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/tiu/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.tiu.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     // Expose a '/' that returns "Hello World"
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";

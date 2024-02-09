631adbd3050536f81580a23249fcfee2a4413dba
TiuDepZai <58964817+TiuDepZai@users.noreply.github.com>
2024-02-09 14:48:53 +1000

    Changing server port and required URL path. Also commented Actuator and Security so no logging in it required each time

--- src/main/java/com/tiu/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/tiu/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,14 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // Expose a '/' that returns "Hello World"
+    // Inject properties for: coach.name and team.name
+
     @Value("${coach.name}")
     private String coachName;
 
     @Value("${team.name}")
     private String teamName;
 
+    // Expose new endpoint for teaminfo
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + ", Team name: "+ teamName;
+    }
+
     // Default Endpoint
+    // Expose a '/' that returns "Hello World"
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
